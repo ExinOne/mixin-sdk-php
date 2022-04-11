80f5bef6af1289478ce4f55db6965db5e17ff500
Rare <rare@exin.one>
2022-04-11 12:18:46 +0800

    添加批量发消息状态

--- tests/Feature/MessageApiTest.php
+++ tests/Feature/MessageApiTest.php
@@ -15,32 +15,34 @@ use PHPUnit\Framework\TestCase;
 class MessageApiTest extends TestCase
 {
     protected $mixinSDK;
+    protected $main_uuid;
 
     public function __construct(string $name = null, array $data = [], string $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
         $this->mixinSDK = new MixinSDK(require './testKeys.php');
+        $this->main_uuid = 'f0e25d0c-fc7b-4fcc-a9f0-760ee7b61ee3';
     }
 
     public function test_it_can_send_text()
     {
-        $id  = '17d1c125-aada-46b0-897d-3cb2a29eb011';
+        $id  = $this->main_uuid;
         $res = $this->mixinSDK->message()->sendText($id, 'sdk send test');
         dump($res);
-        self::assertInternalType('array', $res);
+        self::assertIsArray($res);
     }
 
     public function test_it_can_send_contact()
     {
-        $id  = '17d1c125-aada-46b0-897d-3cb2a29eb011';
+        $id  = $this->main_uuid;
         $res = $this->mixinSDK->message()->sendContact($id, $id);
         dump($res);
-        self::assertInternalType('array', $res);
+        self::assertIsArray($res);
     }
 
     public function test_it_can_send_app_button_group()
     {
-        $id  = '17d1c125-aada-46b0-897d-3cb2a29eb011';
+        $id  = $this->main_uuid;
         $res = $this->mixinSDK->message()->sendAppButtonGroup($id, [
             [
                 'label'  => 'hello',
@@ -49,12 +51,12 @@ class MessageApiTest extends TestCase
             ]
         ]);
         dump($res);
-        self::assertInternalType('array', $res);
+        self::assertIsArray($res);
     }
 
     public function test_it_can_send_app_card()
     {
-        $id  = '17d1c125-aada-46b0-897d-3cb2a29eb011';
+        $id  = $this->main_uuid;
         $res = $this->mixinSDK->message()->sendAppCard($id, [
                 'icon_url'    => 'https://mixin.one/assets/98b586edb270556d1972112bd7985e9e.png',
                 'title'       => 'Mixin',
@@ -63,15 +65,38 @@ class MessageApiTest extends TestCase
             ]
         );
         dump($res);
-        self::assertInternalType('array', $res);
+        self::assertIsArray($res);
     }
 
     public function test_it_can_send_batch_message()
     {
-        $ids = ['17d1c125-aada-46b0-897d-3cb2a29eb011'];
-        $res = $this->mixinSDK->message()->sendBatchMessage($ids, 'A free and lightning fast peer-to-peer transactional network for digital assets.');
+        $ids = [$this->main_uuid];
+        $res = $this->mixinSDK->message()->sendBatchMessage($ids, 'A free and lightning fast peer-to-peer transactional network for digital assets.', true);
         $res2 = $this->mixinSDK->message()->sendBatchMessage($ids, ['A free and lightning fast peer-to-peer transactional network for digital assets.']);
         dump($res,$res2);
-        self::assertInternalType('array', $res);
+        self::assertIsArray($res);
+    }
+
+    public function test_send_batch_message_with_bath_type(){
+        $ids = [$this->main_uuid, $this->main_uuid];
+        $test_button_group = json_encode([
+            [
+                'label'  => 'test label 1',
+                'color'  => '#6777FF',
+                'action' => 'https://www.google.com',
+            ],
+            [
+                'label'  => 'test label 2',
+                'color'  => '#6777FF',
+                'action' => 'https://www.github.com',
+            ]
+
+        ]);
+        $res = $this->mixinSDK->message()->sendBatchMessage($ids, ['batch type send test websocket', $test_button_group],false, ['PLAIN_TEXT', 'APP_BUTTON_GROUP']);
+        $res2 = $this->mixinSDK->message()->sendBatchMessage($ids, ['batch type send test http', $test_button_group],true, ['PLAIN_TEXT', 'APP_BUTTON_GROUP']);
+
+        dump(['res' => $res, 'res2' => $res2]);
+        self::assertIsArray($res);
+
     }
 }
