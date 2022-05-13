7f698d64519542c1237c02722bcfe4b7514fa699
Roay <roachard@hotmail.com>
2022-05-13 15:45:44 +0800

    bug fix

--- src/Container.php
+++ src/Container.php
@@ -85,7 +85,7 @@ use GuzzleHttp\Client;
  * @method  array externalProxy($params, $method = 'sendrawtransaction'): array
  * @method  array postMultisigs(string $raw, string $action = 'sign'): array
  * @method  array multisigsRequests(string $raw, string $action = 'sign'): array
- * @method  array multisigsRequestsSing(string $request_id, string $pin = null): array
+ * @method  array multisigsRequestsSign(string $request_id, string $pin = null): array
  * @method  array multisigsRequestsUnlock(string $request_id, string $pin = null): array
  * @method  array multisigsRequestsCancel(string $request_id, string $pin = null): array
  * @method  array multisigsSign(string $request_id, string $pin = null): array
